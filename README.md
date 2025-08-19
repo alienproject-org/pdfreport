db86e52e2ec8d9f52a56a32098ead28739e3e639
max-brugnerotto <massimiliano.brugnerotto@oesis.it>
2025-08-19 04:19:35 +0200

    Initial commit for alienproject-org

---
+++ README.md
@@ -0,0 +1,2 @@
+# PDFReport
+A library that allows you to generate PDF reports from an XML template describing the document, with placeholders for data
---
+++ src/gaugechart.php
@@ -0,0 +1,31 @@
+<?php 
+
+class GaugeChart
+{
+	public $value = 0.0;
+	public $minValue = 0.0;
+	public $maxValue = 100.0;
+	
+	public $x1 = 0.0;		// mm - Chart container
+	public $y1 = 0.0;		// mm
+	public $x2 = 40.0;		// mm
+	public $y2 = 20.0;		// mm
+	
+	public $orientation = 'V';      // V - vertical
+	
+	function __construct() {
+        //parent::__construct();
+    }
+	
+	public function render()
+	{
+		
+	}
+	
+	private function calculateChartArea()
+	{
+	}
+	
+}
+
+?>
